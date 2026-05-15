d7eeade7de0f92fddd9eed3e1ba4b33fecb0ea75
Cadu <cadurodrigues319@gmail.com>
2026-05-15 15:20:30 -0300

    feat: add modelo Contact e migration com definições de schema, deletes e casts

---
+++ database/migrations/2026_05_15_112430_create_contacts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Validation\Rules\Email;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id(); //coluna ID
+            $table->string('name');
+            $table->string('email')->unique(); //não pode repetir
+            $table->string('phone');
+            $table->integer('score')->default(0);
+            $table->enum('status', ['pending', 'processing', 'active', 'failed'])->default('pending');
+            $table->timestamp('processed_at')->nullable(); // data de quando o score foi calculado 
+            $table->timestamps(); //cria 'created_at' e 'updated_at'
+            $table->softDeletes(); //deletar sem apagar de verdade (deleted_at)
+        });
+    }
+
+
+
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
